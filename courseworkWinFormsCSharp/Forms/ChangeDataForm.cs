fc582193f88a0a2ed8cc480d9bf1a6e38730be7a
Aleksey <89061963710@mail.ru>
2024-01-23 02:41:25 +0700

    Created a Change Data Form and changed the properties of the form

---
+++ courseworkWinFormsCSharp/Forms/ChangeDataForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace courseworkWinFormsCSharp.Forms
+{
+    public partial class ChangeDataForm : Form
+    {
+        public ChangeDataForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
