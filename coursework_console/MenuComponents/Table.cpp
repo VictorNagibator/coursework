e8000dcfc8651623a6deb6a4a72adf9610169399
Victor <vityadonskoy2004@mail.ru>
2024-01-23 15:01:13 +0700

    Fixed table formatting

--- coursework_console/MenuComponents/Table.cpp
+++ coursework_console/MenuComponents/Table.cpp
@@ -6,7 +6,7 @@
 
 void Table::showOrder(Order order) const {
 	showHeader();
-	std::string res = std::format("*{:^14d}*{:^17s}*{:^8s}*{:^22s}*", order.getID(), order.getLaptop().getModelName(), statusTypeToString(order.getStatus()), order.getAdditionalInfo());
+	std::string res = std::format("*{:^14d}*{:^17s}*{:^10s}*{:^22s}*", order.getID(), order.getLaptop().getModelName(), statusTypeToString(order.getStatus()), order.getAdditionalInfo());
 	std::cout << res << std::endl;
 	showFooter();
 }
@@ -16,29 +16,29 @@ void Table::shortShow(std::vector<Order> data) const {
 	int num = 0;
 	for (auto& order : data) {
 		num++;
-		std::string res = std::format("*{:^14d}*{:^17s}*{:^8s}*{:^22s}*", order.getID(), order.getLaptop().getModelName(), statusTypeToString(order.getStatus()), order.getAdditionalInfo());
+		std::string res = std::format("*{:^14d}*{:^17s}*{:^10s}*{:^22s}*", order.getID(), order.getLaptop().getModelName(), statusTypeToString(order.getStatus()), order.getAdditionalInfo());
 		std::cout << res << std::endl;
 		if (num == 5) break;
 	}
-	std::cout << std::format("*{:^14s}*{:^17s}*{:^8s}*{:^22s}*", "...", "...", "...", "...") << std::endl;
+	std::cout << std::format("*{:^14s}*{:^17s}*{:^10s}*{:^22s}*", "...", "...", "...", "...") << std::endl;
 	showFooter();
 }
 
 void Table::fullShow(std::vector<Order> data) const {
 	showHeader();
 	for (auto& order : data) {
-		std::string res = std::format("*{:^14d}*{:^17s}*{:^8s}*{:^22s}*", order.getID(), order.getLaptop().getModelName(), statusTypeToString(order.getStatus()), order.getAdditionalInfo());
+		std::string res = std::format("*{:^14d}*{:^17s}*{:^10s}*{:^22s}*", order.getID(), order.getLaptop().getModelName(), statusTypeToString(order.getStatus()), order.getAdditionalInfo());
 		std::cout << res << std::endl;
 	}
 	showFooter();
 }
 
 void Table::showHeader() const {
-	std::cout << "*******************************************************************" << '\n'
-			  << "* Номер заказа * Название модели * Статус * Дополнительная инфо. *" << '\n'
-		      << "*******************************************************************" << '\n';
+	std::cout << "********************************************************************" << '\n'
+			  << "* Номер заказа * Название модели *  Статус  * Дополнительная инфо. *" << '\n'
+		      << "********************************************************************" << '\n';
 }
 
 void Table::showFooter() const {
-	std::cout << "*******************************************************************" << '\n';
+	std::cout << "********************************************************************" << '\n';
 }
